a2ac54dd3f529635a852dbfd2a80fc9e8963f554
FatemehEi <fatemehebrahimi1998@yahoo.com>
2022-08-22 14:24:53 +0430

    create coin component to show coins

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,19 @@
 import React, {useEffect, useState} from 'react';
 
+//API
 import { getCoins } from '../services/api';
+
+//components
+import Coin from './Coin';
 
 const Landing = () => {
 
-    const [coins, setCoins] = useState();
+    const [coins, setCoins] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
-            const data = await getCoins();    
+            const data = await getCoins(); 
+            console.log("fetch")   
             console.log(data);
             setCoins(data);
         }
@@ -18,6 +23,22 @@ const Landing = () => {
 
     return (
         <div>
+            <input type="text" placeholder="Search" />
+            { coins.length ? 
+                <div>
+                {
+                    coins.map(coin => <Coin 
+                        key={coin.id} 
+                        name={coin.name}
+                        image={coin.image}
+                        symbol={coin.symbol}
+                        price={coin.current_price}
+                        marketCap={coin.market_cap}
+                        priceChange={coin.price_change_percentage_24h} />)
+                }
+                </div> :
+                <h1>Loading ...</h1>
+            }
             
         </div>
     );
